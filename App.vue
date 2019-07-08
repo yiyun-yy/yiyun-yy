20f0ccb1fdd0a025cbfcf541ea6404f6b9f3eaac
yiyun <1042559614@qq.com>
2019-07-08 09:38:51 +0800

    first commit

---
+++ App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+      <div>
+          Vue实例
+          <li v-for="(list,index) in listData" :key="index" @click="handClick(index)">
+            {{list.name}}
+          </li>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      listData:[{
+        name:"lalal"
+      },{
+        name:"wowow"
+      },{
+        name:"dudud"
+      }]
+    }
+  },
+  methods: {
+    handClick(index){
+      console.log(index)
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+
+</style>
